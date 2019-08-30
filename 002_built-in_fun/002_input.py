2891cd1d2da5564359647d0d9691bc8432124f22
Anjan Kumar <41472950+anjan111@users.noreply.github.com>
2019-08-29 17:36:59 -0700

    Add files via upload

---
+++ 002_built-in_fun/002_input.py
@@ -0,0 +1,7 @@
+###########  This is used for to understand input ###########
+
+var = input( "enter some data : ") # reading the data
+print("data with in var : ",var)        # printing the daata using statement
+print(type(var))                       # printing datatype using print and type
+print("memory loc : ",id(var))          # printing memory loc using print and id
+
