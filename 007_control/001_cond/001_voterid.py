9f4398d2d891eb323adc6897ad85fdc99d6cbe10
Anjan Kumar <41472950+anjan111@users.noreply.github.com>
2019-09-23 17:47:57 -0700

    Add files via upload

---
+++ 007_control/001_cond/001_voterid.py
@@ -0,0 +1,18 @@
+# wap for applying voter id application
+
+age = input("enter your age at present : ")
+if( age >= 18 ):
+    print "Fill your personal details"
+    name = input("enter full name : ")
+    DOB = input("enter DOB  : ")
+    mail= input("enter mail : ")
+    submit = input("enter ok  to sumbit your form : ")
+    if(submit == "ok"):
+        print "name : ",name
+        print "DOB  : ",DOB
+        print "mail : ",mail
+    print"Thanks for filling your applcation  form "
+print "Thanks for visiting the voter id application center"
+
+# wap find the area of rectangle when the perimter of the square is odd 
+
